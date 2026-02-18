7b6cd188fa0ea5c6a324c37b405eeb1900e57704
vishnu-510 <vishnukumar5102003@gmail.com>
2026-02-18 23:49:21 +0530

    Add PluginProcessor header

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,1 +1,40 @@
+#pragma once
+#include <JuceHeader.h>
 
+class SlapEnhancerAudioProcessor : public juce::AudioProcessor
+{
+public:
+    SlapEnhancerAudioProcessor();
+    ~SlapEnhancerAudioProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    const juce::String getName() const override;
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int) override;
+    const juce::String getProgramName (int) override;
+    void changeProgramName (int, const juce::String&) override;
+
+    void getStateInformation (juce::MemoryBlock&) override;
+    void setStateInformation (const void*, int) override;
+
+    bool isBusesLayoutSupported (const BusesLayout&) const override;
+
+    juce::AudioProcessorValueTreeState parameters;
+
+private:
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SlapEnhancerAudioProcessor)
+};
